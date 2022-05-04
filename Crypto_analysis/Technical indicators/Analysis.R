3c26c19d5d9e98c9a0713bcfc8b854ad5fc858a9
Cholian <55013018+Cholianlll@users.noreply.github.com>
2022-05-03 22:02:22 -0400

    New token

---
+++ Crypto_analysis/Technical indicators/Analysis.R
@@ -0,0 +1,6 @@
+pacman::p_load(tidyverse)
+
+set.seed(16856)
+
+train_data <- read.csv(file='') # data for prediction
+test_data <- read.csv(file="Clean_data_train.csv") # data for causal mechanisms
